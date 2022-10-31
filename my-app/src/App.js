00c9e37d58615b3e3e33d8e0aae54b4dc3ef98a7
Andrei Grusheuski <gruand13@gmail.com>
2022-10-31 19:12:55 +0300

    make components with function and class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,51 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = ()=>{
+  return <h2>Hello world!</h2>
+}
+
+// const Field =()=>{
+//   const holder = "Enter here"
+//   const styledField={
+//     width:'300px'
+//   }
+//   return <input placeholder={holder} 
+//   type='text' 
+//   style={styledField}/>
+// }
+
+class Field extends Component {
+  render(){
+    const holder = "Enter here"
+    const styledField={
+    width:'300px'
+  }
+    return <input placeholder={holder} 
+    type='text' 
+    style={styledField}/>
+  }
+}
+
+function Btn(){
+  const text="Log in"
+  // const res=()=>{
+  //   return "Log in"
+  // }
+  // const p=<p>LOG in</p>
+  const logged = true;
+  return <button>{logged ? "Enter" : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
+
     </div>
   );
 }
-
+export {Header};
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,23 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Header} from './App';
 
 
-const text = "hello world";
 
-const elem = (
-  <div>
-    <h2 className='text'>Text : {text}</h2>;
-    <input type='text'/>
-    <button tabIndex='0'>click</button>
-  </div>
-);
 
 // const elem = React.createElement('h2', {className: 'greeting'}, 'Hello world');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/>
 );
 
 
